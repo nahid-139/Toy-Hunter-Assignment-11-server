cf89a600c4bf823a7407dbdfe41242f1458a3c46
nahid139 <nahidnhptake@gmail.com>
2023-05-18 07:32:35 -0700

    MongoDb added

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-// 
+require("dotenv").config();
+
+//
 app.use(cors());
 app.use(express.json());
-const toy = require('./Toy.json');
 
-app.get('/', (req, res) => {
-    res.send('Toy Hunter is running')
-});
-app.get('/toy', (req, res) => {
-    res.send(toy);
+
+
+// nahidnhptake
+// Y5Fa6HaAsGJf7sZW
+
+
+
+
+
+const uri = "mongodb+srv://nahidnhptake:Y5Fa6HaAsGJf7sZW@cluster0.krpkhzs.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("toy hunter is running");
+});
 
 app.listen(port, () => {
-    console.log(`Toy hunter server is Running${port}`);
-  });
+  console.log(`Toy hunter server Running${port}`);
+});
