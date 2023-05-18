19832c87f6d89403f798e5adc62121af3ad97159
nahid139 <nahidnhptake@gmail.com>
2023-05-18 04:29:04 -0700

    toy json is running

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const port = process.env.PORT || 5000;
 // 
 app.use(cors());
 app.use(express.json());
+const toy = require('./Toy.json');
 
 app.get('/', (req, res) => {
     res.send('Toy Hunter is running')
 });
+app.get('/toy', (req, res) => {
+    res.send(toy);
+});
+
+
 app.listen(port, () => {
     console.log(`Toy hunter server is Running${port}`);
   });
