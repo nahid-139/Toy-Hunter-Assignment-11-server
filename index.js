319bae5ec084ae929b8e094424ec4d385d24f275
nahid139 <nahidnhptake@gmail.com>
2023-05-18 03:56:30 -0700

    toy server running check

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+// 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Toy Hunter is running')
+});
+app.listen(port, () => {
+    console.log(`Toy hunter server is Running${port}`);
+  });
