b801bbaaa2f267dd23b7f2d90047a48ee3c19dc6
nahid139 <nahidnhptake@gmail.com>
2023-05-18 07:34:34 -0700

    Environment added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://nahidnhptake:Y5Fa6HaAsGJf7sZW@cluster0.krpkhzs.mongodb.net/?retryWrites=true&w=majority";
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.krpkhzs.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
