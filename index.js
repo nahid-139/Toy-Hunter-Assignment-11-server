1a654c08d74dcb17d3557fef8db25b19d49e2942
nahid139 <nahidnhptake@gmail.com>
2023-05-19 05:07:18 -0700

    json add on mongoDB

--- index.js
+++ index.js
@@ -23,11 +23,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   client.connect();
+    const toysCollection = client.db("toyDb").collection("Toy");
+   
+
+    app.get("/toys", async (req, res) => {
+      const toy= toysCollection.find();
+      const result = await toy.toArray();
+      res.send(result);
+    });
+
+   
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
